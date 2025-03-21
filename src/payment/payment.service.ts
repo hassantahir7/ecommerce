7c91a80641abb7c832f13688bd5e4aca7ea6976d
HassantahirDev <hassant1409@gmail.com>
2025-03-21 18:33:44 +0500

    fixes

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -13,7 +13,7 @@ export class PaymentService {
     private readonly prismaService: PrismaService,
   ) {
     this.stripe = new Stripe(this.configService.get<string>('STRIPE_SECRET_KEY'), {
-      apiVersion: '2024-12-18.acacia',
+      apiVersion: '2025-02-24.acacia',
     });
   }
 
